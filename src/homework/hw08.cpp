f0eeb01595acaf6e33d7cadfe4397b4622e9ad0a
Apple_pine <appchen0517@gmail.com>
2026-09-09 22:16:33 +0800

    [Add] 112062212

--- src/homework/hw08.cpp
+++ src/homework/hw08.cpp
@@ -4,6 +4,7 @@ static bool ID8() {
     Vec V;
 
     // Add your student ID and message here, in ascending student ID order.
+    V.emplace_back("112062212", "Want to learn interesting and useful knowledge : D");
     V.emplace_back("123000009", "Example student: I want to learn C++ and Git.");
 
     return isGoodVector(8, V);
