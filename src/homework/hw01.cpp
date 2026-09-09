91c28a7bbc4e989e0b7932d7cc64f3597e0ef696
tmouse <tmouse@casehsu.org>
2026-09-09 19:30:25 +0800

    [Add] 112062334

--- src/homework/hw01.cpp
+++ src/homework/hw01.cpp
@@ -4,6 +4,7 @@ static bool ID1() {
     Vec V;
 
     // Add your student ID and message here, in ascending student ID order.
+    V.emplace_back("112062334", "Learn something new on modern C++, I2P only teach something old.");
     V.emplace_back("123000004", "Example student: I want to learn C++ and Git.");
 
     return isGoodVector(1, V);
