6e78fdb0e6ad5e981a01826573dc7a4ed081f6b3
hazcashi <0983943160lin@gmail.com>
2026-09-09 21:42:04 +0800

    [Add] 112062337

--- src/homework/hw04.cpp
+++ src/homework/hw04.cpp
@@ -4,6 +4,7 @@ static bool ID4() {
     Vec V;
 
     // Add your student ID and message here, in ascending student ID order.
+    V.emplace_back("112062337", "teacher and TA are so handsome");
     V.emplace_back("123000003", "Example student: I want to learn C++ and Git.");
 
     return isGoodVector(4, V);
