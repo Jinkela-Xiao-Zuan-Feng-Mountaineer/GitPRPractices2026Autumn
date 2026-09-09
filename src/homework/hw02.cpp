3ea02fccfa9f48bc022b5f252d732887902ac5ad
KuanHsun <benjoe951017@gmail.com>
2026-09-09 19:35:33 +0800

    [Add] 114062328

--- src/homework/hw02.cpp
+++ src/homework/hw02.cpp
@@ -4,6 +4,7 @@ static bool ID2() {
     Vec V;
 
     // Add your student ID and message here, in ascending student ID order.
+    V.emplace_back("114062328", "老師請問可以等四年讓我碩士讀完嗎");
     V.emplace_back("123000005", "Example student: I want to learn C++ and Git.");
 
     return isGoodVector(2, V);
