4bd89f901b7af101a140e9c4f7a92b82fdde5160
lunnn17 <lunnn0917@gmail.com>
2026-09-10 00:49:49 +0800

    [Add] 114062601

--- src/homework/hw04.cpp
+++ src/homework/hw04.cpp
@@ -5,6 +5,7 @@ static bool ID4() {
 
     // Add your student ID and message here, in ascending student ID order.
     V.emplace_back("112062337", "teacher and TA are so handsome");
+    V.emplace_back("114062601", "Hello");
     V.emplace_back("123000003", "Example student: I want to learn C++ and Git.");
 
     return isGoodVector(4, V);
