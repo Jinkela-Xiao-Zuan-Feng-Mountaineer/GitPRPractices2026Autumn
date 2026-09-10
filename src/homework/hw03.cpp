b2dbbbf8f594386882edd799391ed73fbd967cce
Xinsheng Ooi <ooixinsheng@gmail.com>
2026-09-10 10:35:41 +0800

    [Add] 113062362

--- src/homework/hw03.cpp
+++ src/homework/hw03.cpp
@@ -4,6 +4,7 @@ static bool ID3() {
     Vec V;
 
     // Add your student ID and message here, in ascending student ID order.
+    V.emplace_back("113062362", "hello, world");
     V.emplace_back("123000002", "Example student: I want to learn C++ and Git.");
 
     return isGoodVector(3, V);
