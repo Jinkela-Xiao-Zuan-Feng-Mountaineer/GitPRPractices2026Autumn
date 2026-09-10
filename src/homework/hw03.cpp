74385f23d119ba7382dc721b569426194d7a780f
stevennnccc <92087060+stevennnccc@users.noreply.github.com>
2026-09-10 18:21:52 +0800

    [Add] 110062161
    Added a new entry for student ID 110062161 with a message about learning C++.

--- src/homework/hw03.cpp
+++ src/homework/hw03.cpp
@@ -4,6 +4,7 @@ static bool ID3() {
     Vec V;
 
     // Add your student ID and message here, in ascending student ID order.
+    V.emplace_back("110062161", "Learn something new about new C++.");
     V.emplace_back("123000002", "Example student: I want to learn C++ and Git.");
 
     return isGoodVector(3, V);
