574cc9a93c008a31aca6a3333e92b50315db66e9
Estelle <yichia020007@gmail.com>
2026-09-12 02:36:12 +0800

    [Add] 111020007

--- src/homework/hw09.cpp
+++ src/homework/hw09.cpp
@@ -4,6 +4,7 @@ static bool ID9() {
     Vec V;
 
     // Add your student ID and message here, in ascending student ID order.
+    V.emplace_back("111020007", "Gained some practical hands-on experience.");
     V.emplace_back("112062143", "Teacher looks sooo young.");
     V.emplace_back("113062207",
                    "Oh Love, what is Love? How can you look so cute when you're looking at me?");
