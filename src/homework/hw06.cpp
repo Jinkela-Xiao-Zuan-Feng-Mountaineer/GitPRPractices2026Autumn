e86a7d940cd276d8be37f33159c7a88e1a5773a3
jingching666 <andywang20060504@gmail.com>
2026-09-12 10:34:35 +0800

    [Add] 113062118

--- src/homework/hw06.cpp
+++ src/homework/hw06.cpp
@@ -6,6 +6,7 @@ static bool ID6() {
     // Add your student ID and message here, in ascending student ID order.
     V.emplace_back("112062119", "I hope to gain a lot from this course !");
     V.emplace_back("112062313", "I want to enhance my understanding of modern C++.");
+    V.emplace_back("113062118", "I want to learn more coding about jobs.");
     V.emplace_back("123000001", "Example student: I want to learn C++ and Git.");
 
     return isGoodVector(6, V);
