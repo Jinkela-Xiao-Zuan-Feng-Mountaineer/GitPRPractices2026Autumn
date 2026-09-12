6849332e536a8bc2450f9008ba8c7b044ddd1762
owen920831 <owen920831@gmail.com>
2026-09-12 11:36:36 +0800

    [Add] 114062510

--- src/homework/hw05.cpp
+++ src/homework/hw05.cpp
@@ -7,6 +7,7 @@ static bool ID5() {
     V.emplace_back("112030038", "let gooooooooooooooo");
     V.emplace_back("112062239", "oiiaioiiiai");
     V.emplace_back("112062309", "hello");
+    V.emplace_back("114062510", "hi");
     V.emplace_back("123000000", "Example student: I want to learn C++ and Git.");
 
     return isGoodVector(5, V);
