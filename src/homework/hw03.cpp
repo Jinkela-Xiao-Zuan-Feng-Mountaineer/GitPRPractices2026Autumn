0867d547120273d339edc6272c8f4a2e98d0f42f
mingchi <mingchichen1213@gmail.com>
2026-09-12 19:58:53 +0800

    [Add] 112062103

--- src/homework/hw03.cpp
+++ src/homework/hw03.cpp
@@ -5,6 +5,7 @@ static bool ID3() {
 
     // Add your student ID and message here, in ascending student ID order.
     V.emplace_back("110062161", "Learn something new about new C++.");
+    V.emplace_back("112062103", "Pass my C++ course with at least a C+.");
     V.emplace_back("112062109", "Can I pass this course? I hope so.");
     V.emplace_back("113062311", "Good morning.");
     V.emplace_back("113062362", "hello, world");
