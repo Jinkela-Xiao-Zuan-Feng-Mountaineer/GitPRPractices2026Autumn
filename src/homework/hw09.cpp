a7c67591c8d775303771979ccf9716c05c49868b
Kai Tsao <kaihsuant@gmail.com>
2026-09-12 17:47:11 +0800

    [Add] 112062112

--- src/homework/hw09.cpp
+++ src/homework/hw09.cpp
@@ -5,6 +5,7 @@ static bool ID9() {
 
     // Add your student ID and message here, in ascending student ID order.
     V.emplace_back("111020007", "Gained some practical hands-on experience.");
+    V.emplace_back("112062112", "Please add me to the class thru extra selection QAQ");
     V.emplace_back("112062143", "Teacher looks sooo young.");
     V.emplace_back("113062207",
                    "Oh Love, what is Love? How can you look so cute when you're looking at me?");
