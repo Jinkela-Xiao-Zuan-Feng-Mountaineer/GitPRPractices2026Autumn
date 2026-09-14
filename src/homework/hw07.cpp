55f4cc25d6db96d8ce5aa25b6d0f3e3dbda892fc
peteryoyoman <peter1102peter1102@gmail.com>
2026-09-14 11:05:03 +0800

    [Add] zr_11420CS3424

--- src/homework/hw07.cpp
+++ src/homework/hw07.cpp
@@ -5,6 +5,8 @@ static bool ID7() {
 
     // Add your student ID and message here, in ascending student ID order.
     V.emplace_back("123000008", "Example student: I want to learn C++ and Git.");
+    V.emplace_back("zr_11420CS3424",
+                   "Hope to get a better grasp of modern C++ and large project management.");
 
     return isGoodVector(7, V);
 }
