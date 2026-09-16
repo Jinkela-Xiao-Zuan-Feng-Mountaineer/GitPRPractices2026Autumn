76a6988190cda1fcb0d14accafaba14d86004c2e
boy <boy@B0Y.>
2026-09-16 20:28:03 +0800

    [Add] 109350025

--- src/homework/hw06.cpp
+++ src/homework/hw06.cpp
@@ -4,6 +4,7 @@ static bool ID6() {
     Vec V;
 
     // Add your student ID and message here, in ascending student ID order.
+    V.emplace_back("109350025", "I want to become a better C++ programmer.");
     V.emplace_back("112062119", "I hope to gain a lot from this course !");
     V.emplace_back("112062313", "I want to enhance my understanding of modern C++.");
     V.emplace_back("113062118", "I want to learn more coding about jobs.");
