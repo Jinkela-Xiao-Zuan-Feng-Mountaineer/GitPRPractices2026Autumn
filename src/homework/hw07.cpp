4696ad59489982698485cc9bf073d10bed36b32a
112062110 <s112062110@m112.nthu.edu.tw>
2026-09-17 16:36:44 +0800

    [Add] 112062110

--- src/homework/hw07.cpp
+++ src/homework/hw07.cpp
@@ -4,6 +4,7 @@ static bool ID7() {
     Vec V;
 
     // Add your student ID and message here, in ascending student ID order.
+    V.emplace_back("112062110", "Learn more about C++ and its advance usage");
     V.emplace_back("114062631", "Snoopy has a lot of chocolate.");
     V.emplace_back("123000008", "Example student: I want to learn C++ and Git.");
     V.emplace_back("zr_11420CS3424",
