63f1f4127771ee4e72c6aac3582ee248e757dbec
tp6ru4z <tp6r4z@gmail.com>
2026-09-19 16:52:10 +0800

    [Add] 114062214

--- src/homework/hw04.cpp
+++ src/homework/hw04.cpp
@@ -5,11 +5,11 @@ static bool ID4() {
 
     // Add your student ID and message here, in ascending student ID order.
     V.emplace_back("112062337", "teacher and TA are so handsome");
+    V.emplace_back("114062214", "Cabi Cabi I love Snorlax");
     V.emplace_back("114062317", "I want to learn C++ and Git.");
     V.emplace_back("114062601", "Hello");
     V.emplace_back("115065524", "That's right, Gojo Satoru wins.");
     V.emplace_back("123000003", "Example student: I want to learn C++ and Git.");
-
     return isGoodVector(4, V);
 }
 
