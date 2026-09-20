098b518e62d85bd4ac4484497151a322eabf0582
TWTom <twtomtsai@gmail.com>
2026-09-20 17:28:51 +0800

    [Add] 114000110

--- src/homework/hw03.cpp
+++ src/homework/hw03.cpp
@@ -11,6 +11,7 @@ static bool ID3() {
     V.emplace_back("112062305", "EZ");
     V.emplace_back("113062311", "Good morning.");
     V.emplace_back("113062362", "hello, world");
+    V.emplace_back("114000110", "I would like to learn more modern C++ and some performance tips.");
     V.emplace_back("114062107", "I hope I can still have sleep time.");
     V.emplace_back("123000002", "Example student: I want to learn C++ and Git.");
 
