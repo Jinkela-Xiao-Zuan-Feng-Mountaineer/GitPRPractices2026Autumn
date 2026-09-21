131742c86e7a8bde9ec533d7904396c3916b2d78
YoKai95 <yokai16888@gmail.com>
2026-09-21 19:53:37 +0800

    [Add] 113020038
    [Add] 113020038

--- src/homework/hw01.cpp
+++ src/homework/hw01.cpp
@@ -6,6 +6,7 @@ static bool ID1() {
     // Add your student ID and message here, in ascending student ID order.
     V.emplace_back("112030041", "676767676767676767");
     V.emplace_back("112062334", "Learn something new on modern C++, I2P only teach something old.");
+    V.emplace_back("113020038", "I want to learn more about modern C++ and how to build a project");
     V.emplace_back("114065515", "I want to improve my C++ skills.");
     V.emplace_back("123000004", "Example student: I want to learn C++ and Git.");
 
