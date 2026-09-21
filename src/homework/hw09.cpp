47298a4fdcdc30f5a08f60aee648ea4ae021aa72
laurashion <laura@gapp.nthu.edu.tw>
2026-09-21 17:34:02 +0800

    [Add] 112006204

--- src/homework/hw09.cpp
+++ src/homework/hw09.cpp
@@ -5,6 +5,7 @@ static bool ID9() {
 
     // Add your student ID and message here, in ascending student ID order.
     V.emplace_back("111020007", "Gained some practical hands-on experience.");
+    V.emplace_back("112006204", "I want to learn more about Git :)");
     V.emplace_back("112062112", "Please add me to the class thru extra selection QAQ");
     V.emplace_back("112062143", "Teacher looks sooo young.");
     V.emplace_back("112062224", "Hope to learn more.");
