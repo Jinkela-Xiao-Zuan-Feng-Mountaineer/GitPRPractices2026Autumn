b4c3196a95d1400cbec7b53fcd4305f7c9cf998c
113062301 <kawahehehaha@gapp.nthu.edu.tw>
2026-09-22 19:25:25 +0800

    [Add] 113062301

--- src/homework/hw04.cpp
+++ src/homework/hw04.cpp
@@ -5,6 +5,7 @@ static bool ID4() {
 
     // Add your student ID and message here, in ascending student ID order.
     V.emplace_back("112062337", "teacher and TA are so handsome");
+    V.emplace_back("113062301", "IF I WERE DJ WILL YOU LOVE ME?");
     V.emplace_back("114062214", "Cabi Cabi I love Snorlax");
     V.emplace_back("114062317", "I want to learn C++ and Git.");
     V.emplace_back("114062601", "Hello");
